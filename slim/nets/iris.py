59b3163a4c22b09aa70237e24f44423152230d0a
vonclites <alamos52@gmail.com>
2016-11-22 19:08:49 -0500

    Changed vgg_iris model to operate on larger 64x512 normalized iris images.

--- slim/nets/iris.py
+++ slim/nets/iris.py
@@ -19,30 +19,30 @@ def vgg(inputs,
                         outputs_collections=end_points_collection):
       net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
       net = slim.max_pool2d(net, [2, 2], scope='pool1')
-      # Output shape: (N, 16, 128, 64)
+      # Output shape: (N, 32, 256, 64)
 
       net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
       net = slim.max_pool2d(net, [2, 2], scope='pool2')
-      # Output shape: (N, 8, 64, 128)
+      # Output shape: (N, 16, 128, 128)
 
       net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
       net = slim.max_pool2d(net, [2, 2], scope='pool3')
-      # Output shape: (N, 4, 32, 256)
+      # Output shape: (N, 8, 64, 256)
 
       net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
-      net = slim.max_pool2d(net, [1, 2], stride=[1, 2], scope='pool4')
-      # Output shape: (N, 4, 16, 512)
+      net = slim.max_pool2d(net, [2, 2], stride=[1, 2], scope='pool4')
+      # Output shape: (N, 4, 32, 512)
 
       net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
       net = slim.max_pool2d(net, [2, 2], scope='pool5')
-      # Output shape: (N, 2, 8, 512)
+      # Output shape: (N, 2, 16, 512)
 
-      net = slim.conv2d(net, 4096, [2, 8], padding='VALID', scope='fc6')
+      net = slim.conv2d(net, 500, [2, 16], padding='VALID', scope='fc6')
       net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
                          scope='dropout6')
       # Output shape: (N, 1, 1, 4096)
 
-      net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
+      net = slim.conv2d(net, 500, [1, 1], scope='fc7')
       net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
                          scope='dropout7')
       # Output shape: (N, 1, 1, 4096)
